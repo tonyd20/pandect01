24907c82454c76a977e24944cd57452b8585a6a0
Tony DeBat <tony.debat@gmail.com>
2022-10-11 15:39:31 -0400

    Using the old base58 and expecting failure

--- cmd/pandect.go
+++ cmd/pandect.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcutil/base58"
+)
+
+func SaladBar() {
+	// Encode example data with the modified base58 encoding scheme.
+	data := []byte("Test data")
+	encoded := base58.Encode(data)
+
+	// Show the encoded data.
+	fmt.Println("Encoded Data:", encoded)
+}
 
 func main() {
+	SaladBar()
 	fmt.Println("hello")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tonyd20/pandect01
 
 go 1.19
+
+require github.com/btcsuite/btcutil v1.0.2
